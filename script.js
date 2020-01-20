6bc8e4922070fe19ef93ed4ce4651241adaef551
VERDM028 <Mateo.X.Verdaguer.-ND@disney.com>
2020-01-20 17:57:17 -0300

    20-01-20 code challenge 4

--- script.js
+++ script.js
@@ -208,4 +208,60 @@ console.log(jane);
 /**************
 * Objects and methods
 **************/
+/*
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+    this.age = 2020 - this.birthYear;
+  }
+};
 
+john.calcAge();
+console.log(john);
+*/
+
+/**************
+* CODE CHALLENGE 4
+**************/
+
+// fullname mass height
+
+// method to calc bmi - save bmi to object and return bmi too
+
+// log highest bmi (name and bmi) *consider they can have the same bmi
+
+var mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calculateBmi(){
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+var john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calculateBmi(){
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+mark.calculateBmi();
+john.calculateBmi();
+
+if (john.bmi > mark.bmi) {
+  console.log('John has a higher BMI than Mark, it\'s: ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+  console.log('Mark has a higher BMI than John, it\'s: ' + mark.bmi);
+} else {
+  console.log('Mark and John have the same BMI.');
+}
