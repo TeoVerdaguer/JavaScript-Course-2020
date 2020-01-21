a8c33d8b7423ea4fbfd0697047000fd600864c3e
VERDM028 <Mateo.X.Verdaguer.-ND@disney.com>
2020-01-21 10:56:04 -0300

    21-01-20 loops and iterations

---
+++ 11-loopsAndIteration.js
@@ -0,0 +1,42 @@
+/**************
+* Loops and Iteration
+**************/
+/*
+// for loop
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+for (var i = 0; i < john.length; i++) {
+  console.log(john[i]);
+}
+
+// while loop
+var i = 0;
+while (i < john.lenght) {
+  console.log(john[i]);
+  i++;
+}
+*/
+
+// continue and break statements
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') break;
+  console.log(john[i]);
+}
+
+
+// looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+  console.log(john[i]);
+}
